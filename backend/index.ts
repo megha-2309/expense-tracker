62a1d48c2f15ff562eefa90fcf8f5526f060f1dd
megha-2309 <meghabholla2309@gmail.com>
2026-07-09 19:14:14 +0530

    Cors origin set to true

--- backend/index.ts
+++ backend/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: true,
     credentials: true,
   }),
 );
@@ -54,7 +54,7 @@ app.use(
 const start = async () => {
   await connectDB();
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 };
 
