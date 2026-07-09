cb608aecd364715ff769cd881385c2dec7089a82
megha-2309 <meghabholla2309@gmail.com>
2026-07-09 23:14:35 +0530

    updated

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL ,
     credentials: true,
   }),
 );
